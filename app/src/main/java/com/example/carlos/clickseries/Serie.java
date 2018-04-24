adc071a0ac54191b17347767642652ed055656cc
CarlosDubon <00058016@uca.edu.sv>
2018-04-23 20:11:08 -0600

    Clase serie y diseño tablayout

---
+++ app/src/main/java/com/example/carlos/clickseries/Serie.java
@@ -0,0 +1,50 @@
+package com.example.carlos.clickseries;
+
+public class Serie {
+    private String name;
+    private String description;
+    private String caps;
+    private boolean isFavorite = false;
+
+    public Serie() {
+    }
+
+    public Serie(String name, String description, String caps, boolean isFavorite) {
+        this.name = name;
+        this.description = description;
+        this.caps = caps;
+        this.isFavorite = isFavorite;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCaps(String caps) {
+        this.caps = caps;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCaps() {
+        return caps;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+}
